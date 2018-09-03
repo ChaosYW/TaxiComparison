3d96fa803b4a7f2038b8c4f4c6dc325c27e29d36
ChaosYW <usawangyu@hotmail.com>
2018-09-03 15:55:03 -0400

    Initial commit

---
+++ Taxi/Taxi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Taxi.Startup))]
+namespace Taxi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
