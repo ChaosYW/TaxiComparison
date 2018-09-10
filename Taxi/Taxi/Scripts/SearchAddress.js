c6e91158054715b0b94e7c567cf080773fd6da91
Yu Wang <usawangyu@hotmail.com>
2018-09-10 18:47:41 -0400

    bug fixed for google map

--- Taxi/Taxi/Scripts/SearchAddress.js
+++ Taxi/Taxi/Scripts/SearchAddress.js
@@ -1,17 +1,19 @@
-﻿ 
-function myMap() {
-        var mapProp = {
-        center: new google.maps.LatLng(40.737209, -73.819660),
-    zoom: 12,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+﻿
+$(document).ready(function () {
+    console.log("ready!");
 
+    var mapProp = {
+        center: new google.maps.LatLng(40.737209, -73.819660),
+        zoom: 12,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+});
 
 $("#btSearch").click(function () {
     var addressFrom = $("#iptFrom").val();
     var addressTo = $("#iptTo").val();
     alert(addressFrom);
     alert(addressTo);
-    https://maps.googleapis.com/maps/api/geocode/json?&address=Queens%20college
+
 });
