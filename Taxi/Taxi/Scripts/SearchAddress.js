3829d3e449f738a91fb32d39e918a92d97c1910a
Yu Wang <usawangyu@hotmail.com>
2018-09-11 21:50:04 -0400

    create google map rout display

--- Taxi/Taxi/Scripts/SearchAddress.js
+++ Taxi/Taxi/Scripts/SearchAddress.js
@@ -2,18 +2,39 @@
 $(document).ready(function () {
     console.log("ready!");
 
-    var mapProp = {
-        center: new google.maps.LatLng(40.737209, -73.819660),
+    // Start----------Initial Map function
+
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var defaultCenterMap = new google.maps.LatLng(40.737209, -73.819660);
+
+    var mapOptions = {
         zoom: 12,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        center: defaultCenterMap,
+    }
 
-});
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    directionsDisplay.setMap(map);
 
-$("#btSearch").click(function () {
-    var addressFrom = $("#iptFrom").val();
-    var addressTo = $("#iptTo").val();
-    alert(addressFrom);
-    alert(addressTo);
+    // END----------Initial Map function
 
+    // Calculate Route
+
+    $("#btSearch").click(function () {
+        var addressFrom = $("#iptFrom").val();
+        var addressTo = $("#iptTo").val();
+
+        var request = {
+            origin: addressFrom,
+            destination: addressTo,
+            travelMode: 'DRIVING'
+        };
+
+        directionsService.route(request, function (result, status) {
+            if (status == 'OK') {
+                directionsDisplay.setDirections(result);
+            }
+        });
+    });
+    // END----------calculate route
 });
