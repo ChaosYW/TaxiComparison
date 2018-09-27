caf887400ebd08dbe114bae4a7959a78d6ee4736
Yu Wang <usawangyu@hotmail.com>
2018-09-26 21:51:23 -0400

    get google map address long and lat

--- Taxi/Taxi/Scripts/SearchAddress.js
+++ Taxi/Taxi/Scripts/SearchAddress.js
@@ -43,6 +43,47 @@ $(document).ready(function () {
                 directionsDisplay.setDirections(result);
             }
         });
+
+        var fromlong, fromlat, tolong, tolat;
+       
+        var googleFromURL = "https://maps.googleapis.com/maps/api/geocode/json?address="+addressFrom+"&key=AIzaSyDZ2b3ucOfoCQrm3eqU8qC-1EKP_P2Sh2M";
+
+        $.ajax({
+            type: 'GET',
+            url: googleFromURL,
+            dataType: 'json',
+            success: function (data) {
+                fromlat = data.results[0].geometry.location.lat;
+                fromlong = data.results[0].geometry.location.lng;
+            },
+            async: false
+        });
+
+        
+         
+
+        var googleToURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + addressTo + "&key=AIzaSyDZ2b3ucOfoCQrm3eqU8qC-1EKP_P2Sh2M";
+
+        $.ajax({
+            type: 'GET',
+            url: googleToURL,
+            dataType: 'json',
+            success: function (data) {
+                tolat = data.results[0].geometry.location.lat;
+                tolong = data.results[0].geometry.location.lng;
+            },
+            async: false
+        });
+
+     
+
+        console.log("Start");
+        var url = "https://api.uber.com/v1/estimates/price?start_latitude="+fromlat+"&start_longitude="+fromlong+"&end_latitude="+tolat+"&end_longitude="+tolong+"&server_token=tv52A1T0X3I6osqh7zX4b76O_Usvmth6HQI5QWkp";
+
+        $.getJSON(url, function (data) {
+            console.log(data);
+        });
+        console.log("Hello");
          
     });
     // END----------calculate route
